da9abbca5e57e917244129969bacc23b4e256dd3
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2023-03-29 18:51:52 -0600

    error handling in profileController

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,23 +1,33 @@
 const {Profile} = require('../models/profile');
 const _ = require('lodash');
 
+// get user profile information
 module.exports.getProfile = async (req, res) => {
-    const profile = await Profile.findOne({user: req.user._id});
-    return res.status(200).send(profile);
+    try {
+        const profile = await Profile.findOne({user: req.user._id});
+        return res.status(200).send(profile);
+    } catch (error) {
+        return res.status(400).send({message: "An error occured."});
+    }
 }
 
+// set user profile information
 module.exports.setProfile = async (req, res) => {
-    const userProfile = _.pick(req.body, ["address1", "address2", "phone", "city", "state", "postcode", "country"]);
-    userProfile.user = req.user._id;
+    try {
+        const userProfile = _.pick(req.body, ["address1", "address2", "phone", "city", "state", "postcode", "country"]);
+        userProfile.user = req.user._id;
 
-    let profile = await Profile.findOne({user: req.user._id});
+        let profile = await Profile.findOne({user: req.user._id});
 
-    if (profile) {
-        await Profile.updateOne({user: req.user._id}, userProfile);
-        return res.status(200).send({message: "Your profile info updated successfully!"});
-    } else {
-        profile = new Profile(userProfile);
-        await profile.save();
-        return res.status(201).send({message: "Your profile info saved successfully!"});
+        if (profile) {
+            await Profile.updateOne({user: req.user._id}, userProfile);
+            return res.status(200).send({message: "Your profile info updated successfully."});
+        } else {
+            profile = new Profile(userProfile);
+            await profile.save();
+            return res.status(201).send({message: "Your profile info saved successfully."});
+        }
+    } catch (error) {
+        return res.status(201).send({message: "An error occured."});
     }
 }
